520772062701e5287a6b0fa8fe98f706ba0bc7ee
Andre <andregurgeldev@gmail.com>
2023-02-08 15:48:11 -0300

    project setup

---
+++ src/main/java/br/com/mangakore/mangakorebackend/MangakoreBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.mangakore.mangakorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangakoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangakoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mangakore/mangakorebackend/MangakoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mangakore.mangakorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangakoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
