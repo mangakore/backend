83263b8827d9d783c88fa7e5928f572a37179c6e
Iago Gurgel <iagoggurgel25@gmail.com>
2023-02-10 15:09:05 -0300

    Creating User Model

--- src/main/java/br/com/mangakore/mangakorebackend/api/user/User.java
+++ src/main/java/br/com/mangakore/mangakorebackend/api/user/User.java
@@ -1,6 +1,35 @@
 package br.com.mangakore.mangakorebackend.api.user;
 
 
-public class User {
+import br.com.mangakore.mangakorebackend.api.AuditableEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "user", schema = "security")
+@EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
+public class User extends AuditableEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    @Column(name = "full_name")
+    private String fullName;
 }
